9a6ce2e8ce54322b9f212826232787c44ad4d6ce
gaddalas <srikanth.gaddalapally-non-empl@moodys.com>
2020-07-05 17:28:59 -0400

    Updated travis and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
-docker build -t sri554278/multi-docker-client:latest -f ./client/Dockerfile ./client
-docker build -t sri554278/multi-docker-server:latest -f ./server/Dockerfile ./server
-docker build -t sri554278/multi-docker-worker:latest -f ./worker/Dockerfile ./worker
-
-docker push sri554278/multi-docker-client:latest
-docker push sri554278/multi-docker-server:latest
-docker push sri554278/multi-docker-worker:latest
-
+docker build -t sri554278/multi-docker-client -f ./client/Dockerfile ./client
+docker build -t sri554278/multi-docker-server -f ./server/Dockerfile ./server
+docker build -t sri554278/multi-docker-worker -f ./worker/Dockerfile ./worker
+docker push sri554278/multi-docker-client
+docker push sri554278/multi-docker-server
+docker push sri554278/multi-docker-worker
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=sri554278/multi-docker-server
 kubectl set image deployments/client-deployment client=sri554278/multi-docker-client

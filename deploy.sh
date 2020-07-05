58c24e298246fbb904c8e8bd6f1fb3d691828863
gaddalas <srikanth.gaddalapally-non-empl@moodys.com>
2020-07-05 15:40:10 -0400

    Updated travis and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t sri554278/multi-docker-client:latest -t sri554278/multi-docker-client:$SHA ./client
-docker build -t sri554278/multi-docker-server:latest -t sri554278/multi-docker-server:$SHA ./server
-docker build -t sri554278/multi-docker-worker:latest -t sri554278/multi-docker-worker:$SHA ./worker
+docker build -t sri554278/multi-docker-client:latest -t sri554278/multi-docker-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sri554278/multi-docker-server:latest -t sri554278/multi-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sri554278/multi-docker-worker:latest -t sri554278/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push $DOCKER_HUB_USERID/multi-docker-client:latest
-docker push $DOCKER_HUB_USERID/multi-docker-client:$SHA
+docker push sri554278/multi-docker-client:latest
+docker push sri554278/multi-docker-client:$SHA
 
-docker push $DOCKER_HUB_USERID/multi-docker-server:latest
-docker push $DOCKER_HUB_USERID/multi-docker-server:$SHA
+docker push sri554278/multi-docker-server:latest
+docker push sri554278/multi-docker-server:$SHA
 
-docker push $DOCKER_HUB_USERID/multi-docker-worker:latest
-docker push $DOCKER_HUB_USERID/multi-docker-worker:$SHA
+docker push sri554278/multi-docker-worker:latest
+docker push sri554278/multi-docker-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=$DOCKER_HUB_USERID/multi-docker-server:$SHA
-kubectl set image deployments/client-deployment client=$DOCKER_HUB_USERID/multi-docker-client:$SHA
-kubectl set image deployments/worker-deployment worker=$DOCKER_HUB_USERID/multi-docker-worker:$SHA
+kubectl set image deployments/server-deployment server=sri554278/multi-docker-server:$SHA
+kubectl set image deployments/client-deployment client=sri554278/multi-docker-client:$SHA
+kubectl set image deployments/worker-deployment worker=sri554278/multi-docker-worker:$SHA

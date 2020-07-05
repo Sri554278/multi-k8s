6027ccc9c48d8cbc4c099ffe44e9d90e5ebb4c72
gaddalas <srikanth.gaddalapally-non-empl@moodys.com>
2020-07-05 16:55:17 -0400

    Updated travis and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t sri554278/multi-docker-client:latest -t sri554278/multi-docker-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sri554278/multi-docker-server:latest -t sri554278/multi-docker-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sri554278/multi-docker-worker:latest -t sri554278/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t sri554278/multi-docker-client:latest -t sri554278/multi-docker-client:V1.0 -f ./client/Dockerfile ./client
+docker build -t sri554278/multi-docker-server:latest -t sri554278/multi-docker-server:V1.0 -f ./server/Dockerfile ./server
+docker build -t sri554278/multi-docker-worker:latest -t sri554278/multi-docker-worker:V1.0 -f ./worker/Dockerfile ./worker
 
 docker push sri554278/multi-docker-client:latest
 docker push sri554278/multi-docker-server:latest
 docker push sri554278/multi-docker-worker:latest
 
-docker push sri554278/multi-docker-client:$SHA
-docker push sri554278/multi-docker-server:$SHA
-docker push sri554278/multi-docker-worker:$SHA
+docker push sri554278/multi-docker-client:V1.0
+docker push sri554278/multi-docker-server:V1.0
+docker push sri554278/multi-docker-worker:V1.0
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sri554278/multi-docker-server:$SHA
-kubectl set image deployments/client-deployment client=sri554278/multi-docker-client:$SHA
-kubectl set image deployments/worker-deployment worker=sri554278/multi-docker-worker:$SHA
+kubectl set image deployments/server-deployment server=sri554278/multi-docker-server:V1.0
+kubectl set image deployments/client-deployment client=sri554278/multi-docker-client:V1.0
+kubectl set image deployments/worker-deployment worker=sri554278/multi-docker-worker:V1.0

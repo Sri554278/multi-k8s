5aa38db7f4122c883d149dfd7e550abca88ac335
gaddalas <srikanth.gaddalapally-non-empl@moodys.com>
2020-07-05 18:22:35 -0400

    Push latest

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t sri554278/multi-client:latest -t sri554278/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sri554278/multi-server:latest -t sri554278/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sri554278/multi-worker:latest -t sri554278/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t sri554278/multi-docker-client:latest -t sri554278/multi-docker-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sri554278/multi-docker-server:latest -t sri554278/multi-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sri554278/multi-docker-worker:latest -t sri554278/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push sri554278/multi-client:latest
-docker push sri554278/multi-server:latest
-docker push sri554278/multi-worker:latest
+docker push sri554278/multi-docker-client:latest
+docker push sri554278/multi-docker-server:latest
+docker push sri554278/multi-docker-worker:latest
 
-docker push sri554278/multi-client:$SHA
-docker push sri554278/multi-server:$SHA
-docker push sri554278/multi-worker:$SHA
+docker push sri554278/multi-docker-client:$SHA
+docker push sri554278/multi-docker-server:$SHA
+docker push sri554278/multi-docker-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sri554278/multi-server:$SHA
-kubectl set image deployments/client-deployment client=sri554278/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=sri554278/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=sri554278/multi-docker-server:$SHA
+kubectl set image deployments/client-deployment client=sri554278/multi-docker-client:$SHA
+kubectl set image deployments/worker-deployment worker=sri554278/multi-docker-worker:$SHA
